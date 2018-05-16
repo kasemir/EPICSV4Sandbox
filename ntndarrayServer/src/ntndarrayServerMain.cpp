ad43dce4b3d2244a130605b0c13f74955b28c630
training.Evan <smithej@ornl.gov>
2018-05-16 07:34:10 -0400

    Commented out deprecated *->destroy() calls

--- ntndarrayServer/src/ntndarrayServerMain.cpp
+++ ntndarrayServer/src/ntndarrayServerMain.cpp
@@ -66,8 +66,8 @@ int main(int argc,char *argv[])
     }
 
     pvaServer->shutdown();
-    pvaServer->destroy();
-    channelProvider->destroy();
+    //pvaServer->destroy();
+    //channelProvider->destroy();
     return 0;
 }
 
